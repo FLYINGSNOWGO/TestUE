a73bc99073d68c073ef17c1add91237a49744749
carl.zhou <carlzhou@cloudminds.com>
2022-06-28 20:32:14 +0800

    初始化5.0仓库

---
+++ TestUE5/Source/TestUE5/TestUE5.Build.cs
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TestUE5 : ModuleRules
+{
+	public TestUE5(ReadOnlyTargetRules Target) : base(Target)
+	{
+        PublicIncludePaths.AddRange(
+                new string[] {
+                    "TestUE5"
+                }
+                );
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
